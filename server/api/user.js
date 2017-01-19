a0eb5541cae3e49a133a4511249e83c60f4ea4c4
sunyg <1169771179@qq.com>
2017-01-20 02:13:30 +0800

    bodyparser

--- server/api/user.js
+++ server/api/user.js
@@ -3,11 +3,19 @@ import User from '../controllers/user';
 export default app => {
 
   app.put('/user', (req, res) => {
-    const params = req.query;
+    const params = req.body;
 
     User.addUser(params)
       .then(data => {
-        res.send(data)
+        res.send({
+          code: 0,
+          data
+        });
+      }, err => {
+        res.send({
+          code: 1,
+          err: err.message
+        })
       });
   });
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 // 服务器端路由
 import indexRouter from './routes/index';
 import adminRouter from './routes/admin';
@@ -15,12 +17,16 @@ import {devMiddleWare, hotMiddleware} from './devtools';
 const app = express();
 const port = 3000;
 
+
 if (process.env.NODE_ENV !== 'production') {
   app.use(devMiddleWare);
   app.use(hotMiddleware);
 } else {
   app.use('/static', express.static(__dirname + '/../dist'));
 }
+
+app.use(bodyParser.json());
+
 
 // 模板
 app.set('views', './server/views');
