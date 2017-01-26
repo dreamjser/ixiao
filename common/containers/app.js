dc73a2fbff1cb60d96468ecfd2227c84e42da37c
sunyg <1169771179@qq.com>
2017-01-26 15:06:20 +0800

    navbar放入layouts里

--- common/containers/app.js
+++ common/containers/app.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from '../components/navbar';
+import Navbar from '../components/layouts/navbar';
 
 class App extends Component{
   render(){
